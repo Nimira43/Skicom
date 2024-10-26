faf71ee55637dae851365c977412bec8d7e43414
Nimira43 <n.rabone72@aol.com>
2024-10-26 03:00:18 +0100

    Database all set up and viewable in VS Code

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e145b79346285f8740f5638e2d9e48de6ce7adf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4116dde4771d9fcaf8bd54ad0aa5e00d637bf9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
